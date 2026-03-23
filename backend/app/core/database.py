eed2611e9d5d2bb124de6958f9851675b6b6f3d4
vizz-bob <vizz.bob@gmail.com>
2026-03-23 09:25:20 +0000

    Initial commit: project structure, scripts, examples

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,6 +1,14 @@
+import ssl
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
 from app.core.config import settings
+
+# -------------------------
+# SSL CONTEXT FOR RDS
+# -------------------------
+ssl_context = ssl.create_default_context(
+    cafile="/home/ubuntu/KashiKartCiCd/backend/global-bundle.pem"
+)
 
 # -------------------------
 # ASYNC ENGINE
@@ -8,7 +16,10 @@ from app.core.config import settings
 engine = create_async_engine(
     settings.DATABASE_URL,
     echo=settings.ENVIRONMENT == "development",
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    connect_args={
+        "ssl": ssl_context
+    }
 )
 
 # -------------------------
